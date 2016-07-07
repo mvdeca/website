3b9d883fdc331e84af5c46ab971cbb58b144b853
Avik Jain <avikjain12345@gmail.com>
2016-07-06 23:24:17 -0700

    Fixed responsiveness on mobile

--- public/js/init.js
+++ public/js/init.js
@@ -1,20 +1,24 @@
-$(document).ready(function(){
-  $('#img-container').pushpin({ top: $('#img-container').offset().top, bottom: $('#img-container').offset().top*3 });
-  
-
-  $(window).on('scroll', function() {
-    if(window.pageYOffset < 1.5 * $(window).height()) {
-      $('#content-img1').fadeIn();
-      $('#content-img2').fadeOut();
-      $('#content-img3').fadeOut();
-    } else if(window.pageYOffset < 2.5 * $(window).height()) {
-      $('#content-img1').fadeOut();
-      $('#content-img2').fadeIn();
-      $('#content-img3').fadeOut();
-    } else {
-      $('#content-img1').fadeOut();
-      $('#content-img2').fadeOut();
-      $('#content-img3').fadeIn();
-    }
-  })
+$(document).ready(function() {
+  var $window = $(window);
+  if($window.width() > 1024) {
+    $('#img-container').pushpin({ top: $('#img-container').offset().top, bottom: $('#img-container').offset().top*3 });
+    updateImage();
+    $window.on('scroll', updateImage);
+  }
 });
+
+function updateImage() {
+  if(window.pageYOffset < 1.5 * $(window).height()) {
+    $('#content-img1').fadeIn();
+    $('#content-img2').fadeOut();
+    $('#content-img3').fadeOut();
+  } else if(window.pageYOffset < 2.5 * $(window).height()) {
+    $('#content-img1').fadeOut();
+    $('#content-img2').fadeIn();
+    $('#content-img3').fadeOut();
+  } else {
+    $('#content-img1').fadeOut();
+    $('#content-img2').fadeOut();
+    $('#content-img3').fadeIn();
+  }
+}

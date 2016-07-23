796c506da499132ee6663224e9cbef801e4842a4
sahasd <sahasd@gmail.com>
2016-07-23 15:35:56 -0700

    added mobile support for index

--- routes/index.js
+++ routes/index.js
@@ -41,15 +41,6 @@ router.get('/register', function onRegister (req, res, next) {
   res.render('register');
 });
 
-router.get('/community', function onCommunity (req, res, next) {
-    if (req.user) {
-    res.render('community', {"loggedIn": true, "username" : req.user});
-  }
-  else {
-    res.render('community', {"loggedIn": false, "username" : "null"});
-  }
-});
-
 //send new credentials to server
 router.post('/register', function whenRegister(req, res, next) {
   DB.createUser(req.body.uname, req.body.pass, req.body.fname, req.body.lname, req.body.sid, req.body.em, req.body.mf, req.body.bday, req.body.gday, req.body.stat, req.body.cnum, req.body.text, req.body.shirt, req.body.pfn, req.body.pln, req.body.r, req.body.pm, req.body.pp, req.body.udate, req.body.addr, req.body.zcode);
@@ -63,6 +54,24 @@ router.get('/logout', function atLogout (req, res) {
   req.session.destroy(function (err) {
     res.redirect('/login');
   });
+});
+
+router.get('/community', function onCommunity (req, res, next) {
+    if (req.user) {
+    res.render('community', {"loggedIn": true, "username" : req.user});
+  }
+  else {
+    res.render('community', {"loggedIn": false, "username" : "null"});
+  }
+});
+
+router.get('/competitions', function onCommunity (req, res, next) {
+    if (req.user) {
+    res.render('competitions', {"loggedIn": true, "username" : req.user});
+  }
+  else {
+    res.render('competitions', {"loggedIn": false, "username" : "null"});
+  }
 });
 
 
